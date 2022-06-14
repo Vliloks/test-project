8fff884305f5059816a784ad81bc50378b76658d
Vliloks <vlilialoks@gmail.com>
2022-06-14 19:16:54 +0300

    fin2

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,40 +3,39 @@ import org.apache.spark.sql.SparkSession
 import com.typesafe.config.{Config, ConfigFactory}
 
 
-object Main {
-  def main(args: Array[String]): Unit = {
+object Main extends App {
 
-    val applicationConf: Config = ConfigFactory.load("application.conf")
+  val applicationConf: Config = ConfigFactory.load("application.conf")
 
-    val conf: SparkConf = new SparkConf().setMaster("local[2]").setAppName("SparkApp")
-    val spark: SparkSession = SparkSession.builder().config(conf).getOrCreate()
+  val conf: SparkConf = new SparkConf().setMaster("local[2]").setAppName("SparkApp")
+  val spark: SparkSession = SparkSession.builder().config(conf).getOrCreate()
 
 
 //    Путь к файлам
 //    val dirName = "src/main/resources/generated"
-    val dirName = applicationConf.getString("env.dirName")
+  val dirName = applicationConf.getString("env.dirName")
 
 //    Ожидаемый размер файла
 //    val givenSize= args(0)
-    val givenSize = applicationConf.getString("env.givenSize")
-    val defaultSize = 128
+  val givenSize = applicationConf.getString("env.givenSize")
+  val defaultSize = 128
 
-    var reqSize = try {
-      givenSize.toInt
-    } catch {
-      case e: Exception => defaultSize
-    }
-
-    reqSize = if (reqSize == 0) defaultSize else reqSize
-
-
-    val smallFiles = new SmallFiles(spark)
-    smallFiles.generateSmallFiles(dirName)
-
-    val compactClass = new CompactClass()
-    compactClass.compact(reqSize, dirName, spark)
-    compactClass.saveMetaInfoInDB(dirName, spark)
-
-
+  var reqSize = try {
+    givenSize.toInt
+  } catch {
+    case e: Exception => defaultSize
   }
+
+  reqSize = if (reqSize == 0) defaultSize else reqSize
+
+
+  val smallFiles = new SmallFiles(spark)
+  smallFiles.generateSmallFiles(dirName)
+
+  val compactClass = new CompactClass()
+  compactClass.compact(reqSize, dirName, spark)
+  compactClass.saveMetaInfoInDB(dirName, spark)
+
+  spark.stop()
+
 }
